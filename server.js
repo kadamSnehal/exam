53a32838e024d9a9a830601aab1eafbd1fd082ef
kadamSnehal <snehal9160@gmail.com>
2022-07-31 12:37:01 +0530

    adding files

---
+++ server.js
@@ -0,0 +1,12 @@
+var express=require('express');
+var app=express();
+
+app.get("/",(req,resp)=>{
+    resp.send("<h2>Welcome</h2>");
+});
+
+app.get("/aboutus",()=>{
+    resp.send("<h2>Nothing to describe !!!!!</h2>")
+});
+app.listen(3000);
+console.log("server started at port no 3000");
